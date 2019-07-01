4661da0653bc8e1b1c7fd2ae4cda44a9f5cfa3b8
Jordan Mor <jordanmor@gmail.com>
2019-07-01 14:23:12 -0400

    Create BlogPostRepository/Connect to BlogPostContoller

---
+++ src/main/java/com/tts/blogTutorial/BlogPost/BlogPostRepository.java
@@ -0,0 +1,7 @@
+package com.tts.blogTutorial.BlogPost;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BlogPostRepository extends CrudRepository<BlogPost, Long>{
+	
+}
