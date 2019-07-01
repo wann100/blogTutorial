da095d12c5f2af74c91a8f6e295f1be55d865f45
Jordan Mor <jordanmor@gmail.com>
2019-07-01 15:39:50 -0400

    Set up method to post data to database
    Display for submission data to user on results page
    Rename BlogPostContoller.java to BlogPostController.java

--- src/main/java/com/tts/blogTutorial/BlogPost/BlogPostRepository.java
+++ src/main/java/com/tts/blogTutorial/BlogPost/BlogPostRepository.java
@@ -3,5 +3,5 @@ package com.tts.blogTutorial.BlogPost;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BlogPostRepository extends CrudRepository<BlogPost, Long>{
-	
+
 }
